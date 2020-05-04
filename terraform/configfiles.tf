43072b19e559ff30c58e7e3b3726a3af87cdeb77
Tony Mills <tony@mulini.org>
2020-05-04 23:19:39 +0100

    First commit after upgrade tool run

--- terraform/configfiles.tf
+++ terraform/configfiles.tf
@@ -2,15 +2,15 @@
 # to S3 bucket to be copied by userdata commands during build
 
 data "template_file" "dovecot_ldap_conf" {
-    template = "${file("../configfiles/dovecot/conf.d/dovecot-ldap.conf.ext.tpl")}"
-    vars = {
-        jumpcloud_org = "${var.jumpcloud_org}"
-    }
+  template = file("../configfiles/dovecot/conf.d/dovecot-ldap.conf.ext.tpl")
+  vars = {
+    jumpcloud_org = var.jumpcloud_org
+  }
 }
 
 resource "aws_s3_bucket_object" "dovecot_ldap_conf" {
-    bucket = "${local.domain_name}-configfiles"
-    key = "dovecot/conf.d/dovecot-ldap.conf.ext"
-    content = "${data.template_file.dovecot_ldap_conf.rendered}"
+  bucket  = "${local.domain_name}-configfiles"
+  key     = "dovecot/conf.d/dovecot-ldap.conf.ext"
+  content = data.template_file.dovecot_ldap_conf.rendered
 }
 
--- terraform/gd_lambda.tf
+++ terraform/gd_lambda.tf
@@ -6,30 +6,32 @@ data "archive_file" "zipit" {
   output_path = "../lambda/gd_alerts/processGDAlert.zip"
 }
 
-data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {
+}
 
 resource "aws_lambda_function" "processGDAlert" {
-    filename = "../lambda/gd_alerts/processGDAlert.zip"
-    source_code_hash = "${data.archive_file.zipit.output_base64sha256}"
-    function_name = "processGDAlert"
-    publish = true
-    handler = "processGDAlert.lambda_handler"
-    role = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/lambda_GDAlert"
-    runtime = "python2.7"
+  filename         = "../lambda/gd_alerts/processGDAlert.zip"
+  source_code_hash = data.archive_file.zipit.output_base64sha256
+  function_name    = "processGDAlert"
+  publish          = true
+  handler          = "processGDAlert.lambda_handler"
+  role             = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/lambda_GDAlert"
+  runtime          = "python2.7"
 }
 
 resource "aws_lambda_permission" "processGDAlert_allowCWE" {
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.processGDAlert.function_name}"
+  function_name = aws_lambda_function.processGDAlert.function_name
   principal     = "events.amazonaws.com"
   source_arn    = "arn:aws:events:eu-west-1:${data.aws_caller_identity.current.account_id}:rule/GuardDutyEvent"
-  qualifier = "${aws_lambda_alias.latest_alias.name}"
+  qualifier     = aws_lambda_alias.latest_alias.name
 }
 
 resource "aws_lambda_alias" "latest_alias" {
   name             = "latest_processGDAlert"
   description      = "Process Guard Duty Alert"
-  function_name    = "${aws_lambda_function.processGDAlert.arn}"
+  function_name    = aws_lambda_function.processGDAlert.arn
   function_version = "$LATEST"
 }
+
